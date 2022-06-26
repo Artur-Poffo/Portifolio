17d47adba66506becfdb8268903fdd6e51303472
Artur Poffo <arturpoffoc@hotmail.com>
2022-06-26 19:34:34 -0300

    Projetos e menu

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-const nav = document.querySelector('.navbar')
-let contador = 1
-
-// CARROSEL DE IMAGENS - SECTION PROJETOS
-
-setInterval(function() {
-    document.querySelector('#radio' + contador).checked = true
-    contador ++
-    
-    if(contador > 4) {
-        contador = 1
-    }
-
-}, 3000)
-
-// FIM CARROSEL
+const nav = document.querySelector(".navbar");
+let menu = document.getElementById("menu");
 
 // NAVBAR ANIMAÇÃO:
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY >=3) {
-        nav.classList.add('scrolled')
-    } else {
-        nav.classList.remove('scrolled')
-    }
-})
+  if (window.scrollY >= 3) {
+    nav.classList.add("scrolled");
+    menu.style.background = "#272727";
+  } else {
+    nav.classList.remove("scrolled");
+    menu.style.background = "transparent";
+  }
+});
 
 // FIM NAVBAR ANIMAÇÃO
 
 //MENU-MOBILE
 
-let menu = document.getElementById('menu')
+menu.addEventListener("click", () => {
+  //OPEN MENU
+  document.getElementsByClassName("mobile-list")[0].classList.toggle("open");
 
-menu.addEventListener('click', () => {
-    //OPEN MENU
-    document.getElementsByClassName('mobile-list')[0].classList.toggle('open')
+  //MUDAR .BAR
+  menu.classList.toggle("change");
+});
 
-    //MUDAR .BAR    
-    menu.classList.toggle('change')
+//FIM MENU MOBILE
 
-})
+//CARDS PROJECT
 
+let cards = document.getElementsByClassName("cards-project")[0];
+let cardinfo = document.getElementsByClassName("infocardscont")[0];
+
+cards.onmouseenter = () => {
+    cardinfo.classList.add('hideinfo')
+}
+
+cards.onmouseleave = () => {
+    cardinfo.classList.remove('hideinfo')
+}
